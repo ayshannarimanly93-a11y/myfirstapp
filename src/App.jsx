4ff06ed66077a2a85b2d2650815e14f5ee06d6d2
Aysen <aysen@Aysens-Laptop.local>
2026-04-21 10:27:02 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="myclass">text</h1>
+    <h6>h6text</h6>
+    <button type="button" className='button'>click</button>
+
+
+    </>
+  )
+}
+
+export default App
